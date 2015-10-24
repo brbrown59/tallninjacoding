b5c18cf48288251b5ef705a6741c8f6508056223
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-24 15:24:22 -0600

    Created basic version of the milestone one page

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone One</title>
+	</head>
+	<body>
+		<h1>Milestone One</h1>
+		<h2>Purpose</h2>
+		<p>The purpose of this website is to help further my career by serving as a showcase for my abilities, and a key part of my portfolio</p>
+		<h2>Audience</h2>
+		<p>The intended audience of this project are potential employers, who will hopefully be viewing this as part of a portfolio, in addition to my resume.</p>
+		<p>I would also like this to serve as a showcase for potential clients, should I decide to do some freelance work in the future.</p>
+		<h2>Goals</h2>
+		<p>My main goal for this site is to use it as a tool to gain employment somewhere in the web development field.</p>
+	</body>
+</html>
