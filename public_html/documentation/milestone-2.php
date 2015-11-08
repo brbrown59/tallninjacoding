f38782396b74396ceaed587ccd2630415d92740b
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-08 15:35:09 -0700

    Created milestone 2 page, and added a basic outline

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone Two</title>
+	</head>
+	<body>
+		<h1>Milestone 2</h1>
+		<h2>Persona</h2>
+		<p>stuff</p>
+		<h2>Use Case</h2>
+		<ul>
+			<li>list of stuff</li>
+		</ul>
+		<h2>Sitemap</h2>
+		<p>stuff</p>
+	</body>
+</html>
