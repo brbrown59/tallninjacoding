d7918806a243e91792895de744facd45912ed661
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-11 09:17:36 -0700

    Bulk of the persona finished; need to add his devices

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,9 +8,15 @@
 		<h1>Milestone 2</h1>
 		<h2>Persona</h2>
 		<p>Name: Doug Maloney</p>
-		<p>Mr. Maloney is part of the management team at a mid-sized web development company.  The company is looking to hire
+		<p>Doug is part of the management team at a mid-sized web development company (WebBuilders Inc.).  The company is looking to hire
 			junior developers with a strong core skillset, with the intention of letting them grow into senior devs over time.
+			While the company is willing to give their junior devs plenty of time to grow, they've received many applicants
+			and are trying to narrow the pool down to the ones who are already bringing something to the table.  Also, the
+			volume of applicants, paired with his other responsibilities, means that Doug only has a very limited amount of
+			time to spend evaluating any one applicant.
 		</p>
+		<p>Doug is in his mid-40s, is very tech savvy, and likes having the latest and greatest in technology.  He owns a
+		Samsung Galaxy S6 Edge, </p>
 		<p>Goal: Mr. Maloney's goal is to determine whether or not this person is a good fit for his company.  He needs to
 			be able to decide whether or not this individual is worthy of advancing to the next step of the hiring process
 			within the span of a couple minutes, and with a very cursory look-over of this site.</p>
@@ -19,6 +25,13 @@
 			<li>list of stuff</li>
 		</ul>
 		<h2>Sitemap</h2>
-		<p>The site will</p>
+		<p>The site will consist of three top-level pages:</p>
+		<ul>
+			<li>Home Page</li>
+			<li>About Me</li>
+			<li>Contact</li>
+		</ul>
+		<p>Home: A brief introduction that states, at minimum, my name and role as a full stack developer</p>
+		<p></p>
 	</body>
 </html>
