b0bede6c3aad7fd74bebce7db5eb4d9e6401ec3b
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-11 08:46:45 -0700

    Progress on the persona

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,12 +7,18 @@
 	<body>
 		<h1>Milestone 2</h1>
 		<h2>Persona</h2>
-		<p>stuff</p>
+		<p>Name: Doug Maloney</p>
+		<p>Mr. Maloney is part of the management team at a mid-sized web development company.  The company is looking to hire
+			junior developers with a strong core skillset, with the intention of letting them grow into senior devs over time.
+		</p>
+		<p>Goal: Mr. Maloney's goal is to determine whether or not this person is a good fit for his company.  He needs to
+			be able to decide whether or not this individual is worthy of advancing to the next step of the hiring process
+			within the span of a couple minutes, and with a very cursory look-over of this site.</p>
 		<h2>Use Case</h2>
 		<ul>
 			<li>list of stuff</li>
 		</ul>
 		<h2>Sitemap</h2>
-		<p>stuff</p>
+		<p>The site will</p>
 	</body>
 </html>
