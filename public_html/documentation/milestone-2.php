465db353936b8213a32c248d39942bedce290239
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-11 20:35:51 -0700

    Filled out user interaction, and added small tweaks to the other sections.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -15,17 +15,22 @@
 			volume of applicants, paired with his other responsibilities, means that Doug only has a very limited amount of
 			time to spend evaluating any one applicant.
 		</p>
-		<p>Doug is in his mid-40s, is very tech savvy, and likes having the latest and greatest in technology.  He owns a
-		Samsung Galaxy S6 Edge and a new </p>
+		<p>Doug is in his mid-40s and likes having the latest and greatest in technology.  He owns a Samsung Galaxy S6 Edge and a new Toshiba laptop running Windows 10</p>
 		<p>Goal: Mr. Maloney's goal is to determine whether or not this person is a good fit for his company.  He needs to
 			be able to decide whether or not this individual is worthy of advancing to the next step of the hiring process
 			within the span of a couple minutes, and with a very cursory look-over of this site.</p>
 		<h2>Use Case</h2>
 		<ul>
-			<li>Doug </li>
+			<li>Doug has viewed my resume, and has decided to investigate further by checking out my site</li>
+			<li>Doug navigates to my home page</li>
+			<li>Doug sees the call to action for the portfolio page, and navigates there</li>
+			<li>Doug views the first few projects on my portfolio page, and is impressed</li>
+			<li>Doug uses a link in the header to navigate to the about me/contact page</li>
+			<li>Doug quickly scans my personal summary and notes the list of my core skills</li>
+			<li>Doug decides I am a great fit for a WebBuildersInc. junior developer, and uses my contact info to continue the evaluation process</li>
 		</ul>
 		<h2>Sitemap</h2>
-		<p>The site will consist of three top-level pages:</p>
+		<p>The site will consist of three top-level pages, along with a sitewide navigation bar in the header:</p>
 		<ul>
 			<li>Home Page</li>
 			<li>Portfolio</li>
@@ -33,6 +38,6 @@
 		</ul>
 		<p>Home: A brief introduction that states, at minimum, my name and role as a full stack developer, as well as calls to action for the Portfolio and About Me pages</p>
 		<p>Portfolio: a gallery of my completed projects</p>
-		<p>About Me: a written summary of my education and experience (including a list of my core skills), paired with a contact form and links to any social media profiles</p>
+		<p>About Me: a written summary of my education and experience (including a highly visible list of my core skills), paired with contact details and links to any social media profiles</p>
 	</body>
 </html>
