bbfbc4c23414feb701e4554b84e671ef13e5ae14
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-28 14:17:29 -0700

    Created page for milestone 3

---
+++ public_html/documentation/milestone-3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone Three</title>
+	</head>
+	<body>
+		<h1>Milestone Three</h1>
+		<h2>Homepage</h2>
+		<h3>Desktop</h3>
+		<h3>Mobile</h3>
+		<h2>About Me</h2>
+		<h3>Desktop</h3>
+		<h3>Mobile</h3>
+	</body>
+</html>
