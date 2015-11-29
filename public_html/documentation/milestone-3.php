e748a133c4a0427538c890331d2f32b01f00d8ef
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-28 20:28:06 -0700

    added layouts for the home page

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -8,9 +8,15 @@
 		<h1>Milestone Three</h1>
 		<h2>Homepage</h2>
 		<h3>Desktop</h3>
+		<img src="img/home-desktop.png" alt="Home Layout, Desktop" />
 		<h3>Mobile</h3>
+		<img src="img/home-mobile.png" alt="Home Layout, Mobile" />
 		<h2>About Me</h2>
 		<h3>Desktop</h3>
 		<h3>Mobile</h3>
+		<h2>Other Notes</h2>
+		<p>Color Scheme: charcoal grey/black with white text, possible hints of red used for borders</p>
+		<p>The portfolio layout was not included here (for MVP), and also for the current lack of content.  The buttons to link to them were included in the home page layouts, but can be removed.</p>
+		<p>The contact link in the header menu links to the contact form section of the about me page.</p>
 	</body>
 </html>
