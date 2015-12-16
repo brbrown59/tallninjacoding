a4e3c2b2803b424f0ad6003680cdc87b0bc95122
Bradley Brown <tall.white.ninja@gmail.com>
2015-12-16 12:56:35 -0700

    swiftmailer hooked up (though no confirmation messages

--- public_html/about-me.php
+++ public_html/about-me.php
@@ -93,7 +93,7 @@
 					<h2 id="contact">Contact</h2>
 				</div>
 			</div>
-			<form action="about-me.php" method="post">
+			<form action="mail.php" method="post">
 				<div class="form-group">
 					<label for="emailform">Email address</label>
 					<input type="email" name="email" class="form-control" id="emailform" placeholder="Email">
@@ -107,55 +107,3 @@
 		</div>
 	</body>
 </html>
-
-<?php
-/**
- * sends contents of a simple html form via swiftmailer
- *
- * Bradley Brown <tall.white.ninja@gmail.com>
- */
-
-require_once(dirname(__DIR__) . "/vendor/autoload.php");
-
-try {
-	//$email = $_POST["email"];
-	//$message = $_POST["content"];
-
-	//echo $email;
-
-
-	//compose and send the email
-	//create swift message
-	$swiftMessage = Swift_Message::newInstance();
-
-	$swiftMessage->setFrom([$_POST["email"] => "Inquiry"]);
-
-	$recipients = ["tall.white.ninja@gmail.com"];
-	$swiftMessage->setTo($recipients);
-
-	//attach subject line
-	$swiftMessage->setSubject("Note from personal website");
-
-	//attach the actual message
-	$message = trim($_POST["content"]);
-	$message = filter_var($message, FILTER_SANITIZE_STRING);
-	$swiftMessage->setBody($message, "text/plain");
-
-	//send email via smtp
-	$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
-	$mailer = Swift_Mailer::newInstance($smtp);
-	$numSent = $mailer->send($swiftMessage, $failedRecipients);
-
-	//throw an exception if the number of people who
-	if($numSent !== count($recipients)) {
-		// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
-		throw(new RuntimeException("unable to send email"));
-	}
-
-// report a successful send
-	echo "<div class=\"alert alert-success\" role=\"alert\">Email successfully sent.</div>";
-
-
-} catch(Exception $exception) {
-	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oops!</strong> Unable to send email: " . $exception->getMessage() . "</div>";
-}
